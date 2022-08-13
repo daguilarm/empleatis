20bf516437b0d70a0abbfb08044e46cca70feedb
Damian-portatil <damian.aguilarm@gmail.com>
2022-08-13 20:12:07 +0200

    fixed

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,41 +1,44 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     {{-- home --}}
-    <sitemap>
+    <url>
         <loc>{{ route('home') }}</loc>
         <changefreq>daily</changefreq>
-        <lastmod>{{ date('Y-m-dTH:i:sP') }}</lastmod>
-    </sitemap>
+        <lastmod>{{ date(DATE_ATOM) }}</lastmod>
+        <priority>1</priority>
+    </url>
     {{-- Legal --}}
-    <sitemap>
+    <url>
         <loc>{{ route('legal.cookies') }}</loc>
         <changefreq>monthly</changefreq>
-        <lastmod>{{ date('Y-m-dTH:i:sP', strtotime("first day of previous month")) }}</lastmod>
-    </sitemap>
-    <sitemap>
+        <lastmod>{{ date(DATE_ATOM, strtotime("first day of previous month")) }}</lastmod>
+    </url>
+    <url>
         <loc>{{ route('legal.privacity') }}</loc>
         <changefreq>monthly</changefreq>
-        <lastmod>{{ date('Y-m-dTH:i:sP', strtotime("first day of previous month")) }}</lastmod>
-    </sitemap>
-    <sitemap>
+        <lastmod>{{ date(DATE_ATOM, strtotime("first day of previous month")) }}</lastmod>
+    </url>
+    <url>
         <loc>{{ route('legal.advice') }}</loc>
         <changefreq>monthly</changefreq>
-        <lastmod>{{ date('Y-m-dTH:i:sP', strtotime("first day of previous month")) }}</lastmod>
-    </sitemap>
+        <lastmod>{{ date(DATE_ATOM, strtotime("first day of previous month")) }}</lastmod>
+    </url>
     {{-- Categories --}}
     @foreach(\App\Models\Category::all() as $category)
-        <sitemap>
+        <url>
             <loc>{{ rute_to_category($category) }}</loc>
             <changefreq>daily</changefreq>
-            <lastmod>{{ date('Y-m-dTH:i:sP') }}</lastmod>
-        </sitemap>
+            <lastmod>{{ date(DATE_ATOM) }}</lastmod>
+            <priority>0.75</priority>
+        </url>
     @endforeach
     {{-- Languages --}}
     @foreach(\App\Models\Language::all() as $language)
-        <sitemap>
+        <url>
             <loc>{{ rute_to_language($language) }}</loc>
             <changefreq>daily</changefreq>
-            <lastmod>{{ date('Y-m-dTH:i:sP') }}</lastmod>
-        </sitemap>
+            <lastmod>{{ date(DATE_ATOM) }}</lastmod>
+            <priority>0.75</priority>
+        </url>
     @endforeach
-</sitemapindex>
+</urlset>
