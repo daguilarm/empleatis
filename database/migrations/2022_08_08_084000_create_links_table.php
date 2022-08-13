5d9c4cf7ee0188e179bda65e6d8cb3c9fe3389ad
Damian-portatil <damian.aguilarm@gmail.com>
2022-08-13 15:27:41 +0200

    Update 2022_08_08_084000_create_links_table.php

--- database/migrations/2022_08_08_084000_create_links_table.php
+++ database/migrations/2022_08_08_084000_create_links_table.php
@@ -13,46 +13,23 @@ return new class extends Migration
      */
     public function up()
     {
-        if (app()->environment() === 'testing') {
-            Schema::create('links', function (Blueprint $table) {
-                $table->id();
-                $table->uuid('offer_id');
-                $table->string('referer', 32); // Get referer ID.
-                $table->unsignedBigInteger('category_id');
-                $table->unsignedBigInteger('language_id');
-                $table->unsignedBigInteger('province_id'); // ID from the provinces database
-                $table->string('device_type'); // Desktop, table or mobile
-                $table->string('device_name', 120); // Get the device name.
-                $table->string('robot', 120)->nullable(); // If robot, robot name
-                $table->string('platform', 120); // Get the operating system.
-                $table->string('browser', 120); // Get the operating system.
-                $table->float('salary_year', 10, 2)->nullable();
-                $table->float('ranking', 10, 2); // Ranking
-                $table->string('workday_type'); // config/empleatis
-                $table->timestamps();
-            });
-
-        } else {
-            Schema::connection('analytics')
-            ->create('links', function (Blueprint $table) {
-                $table->id();
-                $table->uuid('offer_id');
-                $table->string('referer', 32); // Get referer ID.
-                $table->unsignedBigInteger('category_id');
-                $table->unsignedBigInteger('language_id');
-                $table->unsignedBigInteger('province_id'); // ID from the provinces database
-                $table->string('device_type'); // Desktop, table or mobile
-                $table->string('device_name', 120); // Get the device name.
-                $table->string('robot', 120)->nullable(); // If robot, robot name
-                $table->string('platform', 120); // Get the operating system.
-                $table->string('browser', 120); // Get the operating system.
-                $table->float('salary_year', 10, 2)->nullable();
-                $table->float('ranking', 10, 2); // Ranking
-                $table->string('workday_type'); // config/empleatis
-                $table->timestamps();
-            });
-        }
-
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('offer_id');
+            $table->string('referer', 32); // Get referer ID.
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('language_id');
+            $table->unsignedBigInteger('province_id'); // ID from the provinces database
+            $table->string('device_type'); // Desktop, table or mobile
+            $table->string('device_name', 120); // Get the device name.
+            $table->string('robot', 120)->nullable(); // If robot, robot name
+            $table->string('platform', 120); // Get the operating system.
+            $table->string('browser', 120); // Get the operating system.
+            $table->float('salary_year', 10, 2)->nullable();
+            $table->float('ranking', 10, 2); // Ranking
+            $table->string('workday_type'); // config/empleatis
+            $table->timestamps();
+        });
     }
 
     /**
@@ -62,10 +39,6 @@ return new class extends Migration
      */
     public function down()
     {
-        if (app()->environment() === 'testing') {
-            Schema::dropIfExists('links');
-        } else {
-            Schema::connection('analytics')->dropIfExists('links');
-        }
+        Schema::dropIfExists('links');
     }
 };
